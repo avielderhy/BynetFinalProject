ffe1b75f98c7f6990fc5209280266e2894097b9f
Aviel Derhy <avielderhy@gmail.com>
2022-11-08 13:19:43 +0200

    test changed

--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@
 
 status=`curl --write-out "%{http_code}\n" --silent --output /dev/null "http://127.0.0.1:5000"`
 if [ "$status" == "200" ];
-then
-echo "The test PASSED"
-else
-echo "The test Failed"
+	then
+	echo "The test PASSED"
+	else
+	echo "The test Failed"
+fi
