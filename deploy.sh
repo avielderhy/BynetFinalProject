8e4c560af89a796079ad21ef49233f0686cd8390
Aviel Derhy <avielderhy@gmail.com>
2022-11-09 17:15:53 +0200

    fix delpoy 4

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,36 @@ TEST_IP="54.81.199.86"
 DEPLOY_IP="44.203.4.212"
 
 MACHINE=$1
-if [ "$MACHINE" == "test" ];
+if [ "$MACHINE" == "test" && $("dig +short myip.opendns.com @resolver1.opendns.com") == $TEST_IP ];
 then
-	MACHINE_IP=$TEST_IP
-else
-	MACHINE_IP=$DEPLOY_IP
-fi
-
-ssh -tt -o StrictHostKeyChecking=no ec2-user@${MACHINE_IP} << 'EOF'
 	cd /home/ec2-user/BynetFinalProject/
 	docker-compose up --no-build -d
 	sleep 5
-	if [ "$MACHINE" == "test" ];
-	then
-		./test.sh
-	fi
+	./test.sh
+fi
+
+if [ "$MACHINE" == "prod" && $("dig +short myip.opendns.com @resolver1.opendns.com") == $DEPLOY_IP ];
+then
+	cd /home/ec2-user/BynetFinalProject/
+	docker-compose up --no-build -d
+fi
+
+if [ "$MACHINE" == "test" && $("dig +short myip.opendns.com @resolver1.opendns.com") == $DEPLOY_IP ];
+then
+	ssh -tt -o StrictHostKeyChecking=no ec2-user@${TEST_IP} << 'EOF'
+		"cd /home/ec2-user/BynetFinalProject/"
+		"docker-compose up --no-build -d"
+	EOF
+fi
+
+else
+	ssh -tt -o StrictHostKeyChecking=no ec2-user@${MACHINE_IP} << 'EOF'
+	cd /home/ec2-user/BynetFinalProject/
+	docker-compose up --no-build -d
+	sleep 5
+	./test.sh
 EOF
+
+
 
 echo "Deployed to $MACHINE machine successfully"
