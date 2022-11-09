de49729f85065e922176462317d18c8c36f82343
Aviel Derhy <avielderhy@gmail.com>
2022-11-09 17:31:42 +0200

    fix deploy 7

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,12 @@ HOME_DIR="/home/ec2-user"
 WORKSPACE="/var/lib/jenkins/workspace/BynetFinalProject"
 TEST_IP="54.81.199.86"
 DEPLOY_IP="44.203.4.212"
+MY_IP = "$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
 MACHINE=$1
-if [ "$MACHINE" == "test"];
+if [ "$MACHINE" == "test" ];
 then
-	if [ $("dig +short myip.opendns.com @resolver1.opendns.com") == $TEST_IP ];
+	if [ $MY_IP =~ $TEST_IP ];
 	then
 	cd /home/ec2-user/BynetFinalProject/
 	docker-compose up --no-build -d
@@ -21,7 +22,7 @@ fi
 
 if [ "$MACHINE" == "prod" ];
 then
-	if [ $("dig +short myip.opendns.com @resolver1.opendns.com") == $DEPLOY_IP ];
+	if [ $MY_IP == $DEPLOY_IP ];
 	then
 	cd /home/ec2-user/BynetFinalProject/
 	docker-compose up --no-build -d
